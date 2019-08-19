622a9e2963b0d6cc8576028671a2e2c6a11b35e2
Aleksandr Matyushik <34347587+AleksanderIII@users.noreply.github.com>
2019-08-19 17:57:06 +0300

    comine models

--- src/models/components/index.ts
+++ src/models/components/index.ts
@@ -3,7 +3,6 @@ export * from './card';
 export * from './input';
 export * from './moneyEditor';
 export * from './select';
-export * from './gridSection';
 export * from './icons';
 export * from './siteManager';
 export * from './tooltip';
--- src/models/containers/grid.ts
+++ src/models/containers/grid.ts
@@ -1,8 +1,14 @@
 import { IMoneyData, Editor } from '../../models/core';
-
+export interface IGridSectionState {
+    top: number;
+}
 export interface IGridProps {
     data: IMoneyData[];
     countryFilter?: string;
     centuryFilter?: string;
     dataType: Editor.Selectors.Options.TYPE;
+}
+export interface IGridSectionProps {
+    countryName: string;
+    countryData: IMoneyData[];
 }
